bfa09c36c5c45ef58fd1f01153a79f0bd1d1af51
EleneJobava <ejoba22@freeuni.edu.ge>
2026-02-22 19:18:40 +0400

    Implemented part of search, calculated bm25 and embedding scores

--- app/search.py
+++ app/search.py
@@ -3,6 +3,7 @@ import re
 from pathlib import Path
 from rank_bm25 import BM25
 from sentence_transformers import SentenceTransformer
+import numpy as np
 
 DEFAULT_RESULTS = 10
 
@@ -17,6 +18,12 @@ def load_products(path: Path) -> list:
     with open(path, "r") as f:
         return json.load(f)
 
+def normalize(scores):
+    min_s, max_s = scores.min(), scores.max()
+    if max_s - min_s == 0:
+        return scores
+    return (scores - min_s) / (max_s - min_s)
+
 class SearchEngine:
     def __init__(self, products: list[dict]) -> None:
         self.products = products
@@ -30,7 +37,23 @@ class SearchEngine:
 
         self.bm25 = BM25(self.tokenized_corpus)
         self.model = SentenceTransformer("all-MiniLM-L6-v2")
-        self.embeddings = self.model.encode(corpus, show_progress_bar=True)
+        self.embeddings = self.model.encode(corpus)
+
+    def search(self, query: str) -> list[dict]:
+        processed_query = handle_input(query)
+        tokens = processed_query.split()
+
+        bm25_scores = self.bm25.get_scores(tokens)
+        query_embedding = self.model.encode([processed_query])
+        semantic_scores = np.dot(self.embeddings, query_embedding.T).flatten()
+
+        bm25_norm = normalize(bm25_scores)
+        combined_scores = 0.6 * bm25_norm + 0.4 * bm25_norm
+
+
+
+
+
 
 
 
